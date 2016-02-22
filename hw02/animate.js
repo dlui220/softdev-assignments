7407a789f5aacd10f55feb12aae6d4e1793b89ca
Derrick Lui <derricklui1@gmail.com>
2016-02-22 14:50:28 -0500

    updated

--- hw02/animate.js
+++ hw02/animate.js
@@ -3,22 +3,38 @@ var ctx = canvas.getContext("2d");
 var radius = 0;
 var growing = true;
 
+
+// Found this function on stack overflow for getting random colors
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 var drawDot = function() {
 
+		ctx.clearRect(0,0,500,500);
+
 		if (growing){
-				radius = radius + 1;
+				radius = radius + 2;
 		} else {
-				radius = radius - 1;
+				radius = radius - 2;
 		}
 		
-		if (radius == 50){
+		if (radius == canvas.width/2){
 				growing = false;
 		} else if ( radius == 0 ){
 				growing = true;
 		}
 
+		if (radius % 20 == 0){
+				ctx.fillStyle = getRandomColor();
+		}
+
 		ctx.beginPath();
-		ctx.fillStyle = "red";
 		ctx.arc(canvas.width/2,canvas.height/2,radius,0,2*Math.PI);
 		ctx.closePath();
 		ctx.fill();
@@ -30,5 +46,4 @@ var b = document.getElementById("button").addEventListener("click",drawDot);
 
 // var b = document.getElementById("button").addEventListener("click",function(e) {
 // 		e.preventDefault();
-// 		ctx.clearRect(0,0,1000,1000);
 // });
