84323157910dfe7a604dc0e88bcc9683415774fe
Derrick Lui <derricklui1@gmail.com>
2016-02-23 00:36:36 -0500

    updated

--- hw02/animate.js
+++ hw02/animate.js
@@ -1,8 +1,10 @@
 var canvas = document.getElementById("playground");
 var ctx = canvas.getContext("2d");
+ctx.fillStyle = "red";
 var radius = 0;
 var growing = true;
-
+var animated = true;
+var clicks = 0;
 
 // Found this function on stack overflow for getting random colors
 function getRandomColor() {
@@ -25,25 +27,30 @@ var drawDot = function() {
 		}
 		
 		if (radius == canvas.width/2){
+				ctx.fillStyle = getRandomColor();
 				growing = false;
 		} else if ( radius == 0 ){
-				growing = true;
-		}
-
-		if (radius % 20 == 0){
 				ctx.fillStyle = getRandomColor();
+				growing = true;
 		}
 
 		ctx.beginPath();
 		ctx.arc(canvas.width/2,canvas.height/2,radius,0,2*Math.PI);
 		ctx.closePath();
 		ctx.fill();
+		if (animated){
+				window.requestAnimationFrame( drawDot );		
+		}
+};
 
-		window.requestAnimationFrame( drawDot );
+var stopDot = function() {
+		if (clicks == 0){
+				animated = false;
+		} else if (clicks == 1){
+				ctx.clearRect(0,0,500,500);
+				clicks = 0;
+		}
 };
 
 var b = document.getElementById("button").addEventListener("click",drawDot);
-
-// var b = document.getElementById("button").addEventListener("click",function(e) {
-// 		e.preventDefault();
-// });
+var s = document.getElementById("stop").addEventListener("click",stopDot);
