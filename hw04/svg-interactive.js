e3f2e8fc0d64ab77339e67475909a6bea0d641b4
Derrick Lui <derricklui1@gmail.com>
2016-02-29 12:32:35 -0500

    updated

---
+++ hw04/svg-interactive.js
@@ -0,0 +1,19 @@
+var pic = document.getElementById("vimage");
+
+var c = document.createElementNS(
+    "http://www.w3.org/2000/svg", "circle"
+);
+c.setAttribute( "cx", 0 );
+c.setAttribute( "cy", 0 );
+c.setAttribute( "r", "100" );
+c.setAttribute( "fill", "red" );
+c.setAttribute( "stroke", "black");
+
+var clicked = function(e){
+    if ( e.toElement == this ) {
+	drawDot ( e.offsetX, e.offsetY );
+    }
+};
+
+pic.appendChild( c );
+
