1e9728cad105a4ad24556f46bd7af20d4422ac07
Derrick Lui <derricklui1@gmail.com>
2016-03-01 12:36:14 -0500

    updated

--- hw04/svg-interactive.js
+++ hw04/svg-interactive.js
@@ -1,7 +1,22 @@
 var pic = document.getElementById("vimage");
+var intervalID;
+
+var grow = function() {
+		//init stuff
+		var c = document.createElementNS(svgNSID,"circle");
+		var radius;
+		var animateCode = function(){
+				//increment/decrement code
+				c = document.getElementByTagName("circle")[0];
+				radius = parseInt(c.getAttribute("r"));
+				c.setAttribute("r",radius.toString());
+		};
+
+		intervalID = window.setInterval(animateCode,16);
+};
 
 var c = document.createElementNS(
-    "http://www.w3.org/2000/svg", "circle"
+		"http://www.w3.org/2000/svg", "circle"
 );
 c.setAttribute( "cx", 0 );
 c.setAttribute( "cy", 0 );
@@ -10,9 +25,13 @@ c.setAttribute( "fill", "red" );
 c.setAttribute( "stroke", "black");
 
 var clicked = function(e){
-    if ( e.toElement == this ) {
-	drawDot ( e.offsetX, e.offsetY );
-    }
+		if ( e.toElement == this ) {
+				drawDot ( e.offsetX, e.offsetY );
+		}
+};
+
+var stop = function(){
+		window.clearInterval(intervalID);
 };
 pic.addEventListener( "click", clicked );
 pic.appendChild( c );
