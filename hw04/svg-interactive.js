627b2355dee44dc1f947ffb5fe1e9cb51be9a978
Derrick Lui <derricklui1@gmail.com>
2016-03-02 00:29:28 -0500

    works w circle

--- hw04/svg-interactive.js
+++ hw04/svg-interactive.js
@@ -1,26 +1,28 @@
 var pic = document.getElementById("csvg");
-// var c = Document.createElementNS('http://www.w3.org/2000/svg','image');
+var button = document.getElementById("cbutton");
+var cbutton_clicked = false;
+var intervalID;
 
 var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
+
 c.setAttribute("cx", 250);
 c.setAttribute("cy", 250);
 c.setAttribute("r", 0);
 c.setAttribute("fill","red");
 c.setAttribute("stroke","black");
 
-var intervalID;
-
-var grow = function() {
-		var growing = true;
-		// var c = document.createElementNS(svgNSID,"circle");
-		var animateCode = function(){
-				//increment/decrement code
+var animateCircle = function animateCircle(){
+		var speed = 2;
+    var growing = true;
+    var drawCircle = function drawCircle(){
 				radius = parseInt(c.getAttribute("r"));
-				if (growing) {
-						radius = radius + 1;
+				
+				// Increment/decrement code
+				if (growing == true) {
+						radius = radius + speed;
 				} else {
-						radius = radius - 1;
-				}
+						radius = radius - speed;
+				} 
 				
 				if (radius == 250){
 						growing = false;
@@ -28,38 +30,20 @@ var grow = function() {
 						growing = true;
 				}
 
-				//
-				// c = document.getElementByTagName("circle")[0];
-				radius = parseInt(c.getAttribute("r"));
-				c.setAttribute("r",radius.toString());
+				c.setAttribute("r", radius);
 				pic.appendChild(c);
+
 		};
-		intervalID = window.setInterval(animateCode,16);
+		intervalID = window.setInterval(drawCircle, 16);
 };
 
-var cbutton = document.getElementById("cbutton");
-var dbutton = document.getElementById("dbutton");
 
-cbutton.addEventListener("click", function(){
-    if (grow_true == true){
-				circle_grow();
-				grow_true = false;
+button.addEventListener("click", function(){
+    if (cbutton_clicked == false){
+				animateCircle();
+				cbutton_clicked = true;
     }
     else{
 				window.clearInterval(intervalID);
-				grow_true = false;
     }
 });
-
-// var clicked = function(e){
-// 		if ( e.toElement == this ) {
-// 				drawDot ( e.offsetX, e.offsetY );
-// 		}
-// };
-
-// var stop = function(){
-// 		window.clearInterval(intervalID);
-// };
-// pic.addEventListener( "click", clicked );
-// pic.appendChild( c );
-
