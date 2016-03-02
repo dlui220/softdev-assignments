3b78360ddaf2318eb4f8041139309ff421cdd8c8
Derrick Lui <derricklui1@gmail.com>
2016-03-02 00:06:52 -0500

    pls

--- hw04/svg-interactive.js
+++ hw04/svg-interactive.js
@@ -1,38 +1,65 @@
-var pic = document.getElementById("vimage");
+var pic = document.getElementById("csvg");
+// var c = Document.createElementNS('http://www.w3.org/2000/svg','image');
+
+var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
+c.setAttribute("cx", 250);
+c.setAttribute("cy", 250);
+c.setAttribute("r", 0);
+c.setAttribute("fill","red");
+c.setAttribute("stroke","black");
+
 var intervalID;
 
 var grow = function() {
-		//init stuff
-		var c = document.createElementNS(svgNSID,"circle");
-		var radius;
+		var growing = true;
+		// var c = document.createElementNS(svgNSID,"circle");
 		var animateCode = function(){
 				//increment/decrement code
-				c = document.getElementByTagName("circle")[0];
+				radius = parseInt(c.getAttribute("r"));
+				if (growing) {
+						radius = radius + 1;
+				} else {
+						radius = radius - 1;
+				}
+				
+				if (radius == 250){
+						growing = false;
+				} else if (radius == 0){
+						growing = true;
+				}
+
+				//
+				// c = document.getElementByTagName("circle")[0];
 				radius = parseInt(c.getAttribute("r"));
 				c.setAttribute("r",radius.toString());
+				pic.appendChild(c);
 		};
-
 		intervalID = window.setInterval(animateCode,16);
 };
 
-var c = document.createElementNS(
-		"http://www.w3.org/2000/svg", "circle"
-);
-c.setAttribute( "cx", 0 );
-c.setAttribute( "cy", 0 );
-c.setAttribute( "r", "100" );
-c.setAttribute( "fill", "red" );
-c.setAttribute( "stroke", "black");
+var cbutton = document.getElementById("cbutton");
+var dbutton = document.getElementById("dbutton");
 
-var clicked = function(e){
-		if ( e.toElement == this ) {
-				drawDot ( e.offsetX, e.offsetY );
-		}
-};
+cbutton.addEventListener("click", function(){
+    if (grow_true == true){
+				circle_grow();
+				grow_true = false;
+    }
+    else{
+				window.clearInterval(intervalID);
+				grow_true = false;
+    }
+});
 
-var stop = function(){
-		window.clearInterval(intervalID);
-};
-pic.addEventListener( "click", clicked );
-pic.appendChild( c );
+// var clicked = function(e){
+// 		if ( e.toElement == this ) {
+// 				drawDot ( e.offsetX, e.offsetY );
+// 		}
+// };
+
+// var stop = function(){
+// 		window.clearInterval(intervalID);
+// };
+// pic.addEventListener( "click", clicked );
+// pic.appendChild( c );
 
