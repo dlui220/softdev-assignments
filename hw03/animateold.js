fa38082320f4d1f0f8ca79aba54b452e9fd1603f
Derrick Lui <derricklui1@gmail.com>
2016-03-16 12:31:52 -0400

    woo

---
+++ hw03/animateold.js
@@ -0,0 +1,79 @@
+var canvas = document.getElementById("playground");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "red";
+
+var logo = new Image();
+logo.src = "logo_dvd.jpg";
+
+var xcor = 100;
+var ycor = 400;
+var xreverse = 1;
+var yreverse = 1;
+
+var radius = 0;
+var growing = true;
+var requestId;
+
+// Note:
+// Speed increases when button is clicked more than once in succession,
+// and to stop animation in this case, the stop button also needs to be clicked
+// multiple times
+var drawDot = function() {
+
+		ctx.clearRect(0,0,500,500);
+
+		if (growing){
+				radius = radius + 2;
+		} else {
+				radius = radius - 2;
+		}
+		
+		if (radius == canvas.width/2){
+				ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16); 
+				growing = false;
+		} else if ( radius == 0 ){
+				ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16); 
+				growing = true;
+		}
+
+		ctx.beginPath();
+		ctx.arc(canvas.width/2,canvas.height/2,radius,0,2*Math.PI);
+		ctx.closePath();
+		ctx.fill();
+
+};
+
+var animateDot = function() {
+		drawDot();
+		requestId = window.requestAnimationFrame(animateDot);
+};
+
+var drawDvd = function() {
+		ctx.clearRect(0,0,500,500);
+		ctx.drawImage(logo,xcor,ycor,75,50);
+		
+		xcor = xcor + (3 * xreverse);
+		ycor = ycor + (1 * yreverse);
+
+		if (xcor > (canvas.width-75) || xcor < 0){
+				xreverse = xreverse * -1;
+		};
+
+		if (ycor > (canvas.height-50) || ycor < 0){
+				yreverse = yreverse * -1;
+		};
+};
+
+var animateDvd = function() {
+		drawDvd();
+		requestId = window.requestAnimationFrame(animateDvd);
+};
+
+var stopAnimation = function() {
+		window.cancelAnimationFrame(requestId);
+};
+
+
+var b = document.getElementById("cbutton").addEventListener("click",animateDot);
+var s = document.getElementById("stop").addEventListener("click",stopAnimation);
+var d = document.getElementById("dbutton").addEventListener("click",animateDvd);
