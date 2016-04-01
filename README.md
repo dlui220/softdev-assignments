73bf436e134f116e9c948e5d142235d3e6e671ee
Derrick Lui <derricklui1@gmail.com>
2016-04-01 06:49:10 -0400

    updated hw

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # softdev-assignments
+
+Sweaters:
+3 S White #2
+M Black #2
+1 XS Maroon #2
+L Black #3
+3 S White #3
--- hw06/lab.py
+++ hw06/lab.py
@@ -32,5 +32,34 @@ def checkPasswordSimple( password ):
         return True
     return False
 
-print checkPasswordSimple("HelloThisIsPassword")
-print checkPasswordSimple("HelloThisIsPassword1")
+# print checkPasswordSimple("HelloThisIsPassword")
+# print checkPasswordSimple("HelloThisIsPassword1")
+
+def checkPasswordStrength( password ):
+    strength = 1;
+    UC_LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    LC_LETTERS="abcdefghijklmnopqrstuvwyxz"
+    NUM="1234567890"
+    SYM=".?!&#,;:-_*"
+    
+    u = [ 1 if x in UC_LETTERS else 0 for x in password ]
+    l = [ 1 if x in LC_LETTERS else 0 for x in password ]
+    n = [ 1 if x in NUM else 0 for x in password ]
+    s = [ 1 if x in SYM else 0 for x in password ]
+    
+    if ((sum(u) > 0) and (sum(l) > 0)):
+        strength += 2
+    if (sum(n) > 0):
+        strength += 1
+    if (sum(s) > 0):
+        strength += 1
+    if (strength > 1):
+        return "From 1-10, your password (" + password + ") strength is: "+ str(strength*2)
+    return "From 1-10, your password (" + password + ") strength is: "+ str(strength)
+
+
+print checkPasswordStrength("hello")    # return 1
+print checkPasswordStrength("hello1")   # return 8
+print checkPasswordStrength("hello_1")  # return 6
+print checkPasswordStrength("Hello1")   # return 8
+print checkPasswordStrength("Hello_1")  # return 10
