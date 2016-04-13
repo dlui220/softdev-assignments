77999d52a55c5a9ad5f45e12e430aa4142f698cb
Derrick Lui <derricklui1@gmail.com>
2016-04-13 12:33:28 -0400

    updated

---
+++ sandbox/memoization/app.py
@@ -0,0 +1,18 @@
+def memoizer(f):
+    memo = {} #hashmap/dict for 0(1) lookup
+    def inner(x):
+        if x not in memo:
+            memo[x] = f(x)
+        return memo[x]
+    return inner
+
+@memoizer
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+print fib(10)
